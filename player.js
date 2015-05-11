65b9009d12e14042f317e4542ff55376ecd4e244
JixaJosh <josh_deen@hotmail.com>
2015-05-11 19:15:10 +1000

    added sprites

--- player.js
+++ player.js
@@ -1,6 +1,22 @@
 var Player = function() 
 {
-this.image= document.createElement("img");
+this.sprite = new Sprite("ChuckNorris.png");
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [0, 1, 2, 3, 4, 5, 6, 7]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [8, 9, 10, 11, 12]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [52, 53, 54,55, 56, 57, 58, 59]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [60, 61, 62, 63, 64]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+           [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+for(var i=0; i<ANIM_MAX; i++)
+{
+           this.sprite.setAnimationOffset(i, -55, -87);
+}
 
 this.position = new Vector2();
 this.position.set = ( 9*35, 0 * 35);
@@ -8,24 +24,61 @@ this.position.set = ( 9*35, 0 * 35);
 this.width= 159;
 this.height= 163;
 
-this.offset = new Vector2();
-this.offset.set = ( -55, -87);
-
 this.velocity = new Vector2();
 
 this.jumping = false;
 this.falling = true;
 
-
-
-this.image.src= "hero.png";
+his.direction = LEFT;
 };
+
+var LEFT = 0;
+var RIGHT = 1;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_SHOOT_LEFT = 3;
+var ANIM_CLIMB = 4;
+var ANIM_IDLE_RIGHT = 5;
+var ANIM_JUMP_RIGHT = 6;
+var ANIM_WALK_RIGHT = 7;
+var ANIM_SHOOT_RIGHT = 8;
+var ANIM_MAX = 9;
 
 
 Player.prototype.update= function(deltaTime)
 {
 
+this.sprite.update(deltaTime);
 
+if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true) {
+       left = true;
+       this.direction = LEFT;
+       if(this.sprite.currentAnimation != ANIM_WALK_LEFT)
+             this.sprite.setAnimation(ANIM_WALK_LEFT);
+}
+
+if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
+       right = true;
+       this.direction = RIGHT;
+       if(this.sprite.currentAnimation != ANIM_WALK_RIGHT)
+             this.sprite.setAnimation(ANIM_WALK_RIGHT);
+}
+else {
+       if(this.jumping == false && this.falling == false)
+       {
+             if(this.direction == LEFT) 
+             {
+                 if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+                 this.sprite.setAnimation(ANIM_IDLE_LEFT);
+             }
+             else
+             {
+                 if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+                 this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+             }
+        }
+}
 
 var left = false;
 var right = false;
@@ -69,6 +122,10 @@ if (left)
 	 {
 	 ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
 	 this.jumping = true;
+	 if(this.direction == LEFT)
+          this.sprite.setAnimation(ANIM_JUMP_LEFT)
+     else
+          this.sprite.setAnimation(ANIM_JUMP_RIGHT
 	 }
 
 
@@ -163,9 +220,11 @@ if (this.velocity.y > 0)
 }
 Player.prototype.draw= function()
 	{
+    this.sprite.draw(context, this.position.x, this.position.y)	
+
 	context.save();
 	context.translate(this.x, this.y);
 	//context.rotate(this.rotation);
 	context.drawImage(this.image, this.position.x , this.position.y);
 	context.restore();
-	}
+}
