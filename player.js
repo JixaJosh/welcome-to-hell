033c52c3ee0b93a2e8bf4be1723e0e256df38e55
JixaJosh <josh_deen@hotmail.com>
2015-05-13 21:40:18 +1000

    only problem is slide left and map

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 var Player = function() 
 {
 this.sprite = new Sprite("ChuckNorris.png");
+
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
            [0, 1, 2, 3, 4, 5, 6, 7]);
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
@@ -8,13 +9,17 @@ this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
            [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
-           [52, 53, 54,55, 56, 57, 58, 59]);
+           [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
+           [41, 42, 43,44, 45, 46, 47, 48, 49, 50, 51]);
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
            [52, 53, 54,55, 56, 57, 58, 59]);
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05, 
            [60, 61, 62, 63, 64]);
 this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
            [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+           [79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92]);
 for(var i=0; i<ANIM_MAX; i++)
 {
            this.sprite.setAnimationOffset(i, -55, -87);
@@ -45,7 +50,7 @@ var ANIM_IDLE_RIGHT = 5;
 var ANIM_JUMP_RIGHT = 6;
 var ANIM_WALK_RIGHT = 7;
 var ANIM_SHOOT_RIGHT = 8;
-var ANIM_MAX = 7;
+var ANIM_MAX = 9;
 
 
 Player.prototype.update= function(deltaTime)
